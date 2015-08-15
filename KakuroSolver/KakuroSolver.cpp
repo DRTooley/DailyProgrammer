682f54b2e2b432b8a57bad5da17ed6affd29fedf
David Tooley <David.Tooley@live.com>
2015-08-15 00:01:23 -0500

    Progress made but unable to to test because of erroneous output

--- KakuroSolver/KakuroSolver.cpp
+++ KakuroSolver/KakuroSolver.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
+/*****************************************************************************************/
+// I wish to understand the below code - I get to test my debug skills
+// The below template was taken from Marius in:
+// http://stackoverflow.com/questions/236129/split-a-string-in-c
+
+template < class ContainerT >
+void tokenize(const std::string& str, ContainerT& tokens,
+              const std::string& delimiters = " ", bool trimEmpty = false)
+{
+   std::string::size_type pos, lastPos = 0;
+
+   using value_type = typename ContainerT::value_type;
+   using size_type  = typename ContainerT::size_type;
+
+   while(true)
+   {
+      pos = str.find_first_of(delimiters, lastPos);
+      if(pos == std::string::npos)
+      {
+         pos = str.length();
+
+         if(pos != lastPos || !trimEmpty)
+            tokens.push_back(value_type(str.data()+lastPos,
+                  (size_type)pos-lastPos ));
+
+         break;
+      }
+      else
+      {
+         if(pos != lastPos || !trimEmpty)
+            tokens.push_back(value_type(str.data()+lastPos,
+                  (size_type)pos-lastPos ));
+      }
+
+      lastPos = pos + 1;
+   }
+}
+/****************************************************************************************/
 
 
 
@@ -14,9 +53,12 @@ int main(int argc, char* argv[]){
 	}
 	std::ifstream PuzzleFile(argv[0]);
 	std::string PuzzleFileLine;
+	std::vector<std::string> TokenSlot;
 	if(PuzzleFile.is_open()){
 		while(std::getline(PuzzleFile, PuzzleFileLine)){
-			
+			tokenize(PuzzleFileLine, TokenSlot);
+			std::cout << PuzzleFileLine << std::endl;
+			std::cout << TokenSlot[0] << std::endl;
 		}
 	}
 	else{
